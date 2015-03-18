4e17618e455a7f0c44eb8818f8f0ea726eae3780
BobSmithIV <bobsmith0030+git@gmail.com>
2015-03-18 02:14:42 +0000

    Added more comments

--- TagPro Favicon Flair.user.js
+++ TagPro Favicon Flair.user.js
@@ -7,29 +7,34 @@
 // @version      0.1
 // @grant        GM_getValue
 // @grant        GM_setValue
-// @downloadURL  https://raw.githubusercontent.com/BobSmithIV/TagProFaviconFlair/master/TagPro%20Favicon%20Flair.user.js
+// @downloadURL  https://raw.githubusercontent.com/BobSmithIV/TagProFaviconFlair/master/TagProFaviconFlair.user.js
 // ==/UserScript==
 
+// initialize flair (called first time the userscript is run only):
 if(!GM_getValue('currentFlair')){
     GM_setValue('currentFlair','initial');
 }
 
+// change the favicon to the current flair:
 changeFavicon(GM_getValue('currentFlair'));
 
+// if the user clicks while on the profile, check to see if they changed their flair
 $('body').click(function(){
-  if (document.URL.search('profile') >= 0){
-    elements = document.getElementsByTagName('input');
+  if (document.URL.search('profile') >= 0){ // if on the profile page,
+    
+    elements = document.getElementsByTagName('input'); 
     for(var i=0; i<elements.length; i++) {
-        if(elements[i].name=="selectedFlair" && elements[i].checked){
-            if (GM_getValue('currentFlair')!=elements[i].value){
-                changeFavicon(elements[i].value);
+        if(elements[i].name=="selectedFlair" && elements[i].checked){ // if this is a selected flair radio,
+            if (GM_getValue('currentFlair')!=elements[i].value){ // if the selected flair is different to what it was before
+                changeFavicon(elements[i].value); // change the favicon 
+                GM_setValue('currentFlair',elements[i].value); // save the new value
             }
-            GM_setValue('currentFlair',elements[i].value);
         }
     }
 }
 });
 
+// changes the favicon to the inputted flair image, as hosted by myself on GitHub
 function changeFavicon(flair) {
     var link = document.createElement('link');
     var oldLink = document.getElementById('dynamic-favicon');
